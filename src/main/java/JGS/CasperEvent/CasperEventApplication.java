69f0674008f9616dcaf688e25c5279a7d0e3210d
k000927 <kimsungjin927@gmail.com>
2024-07-25 17:55:57 +0900

    init

---
+++ src/main/java/JGS/CasperEvent/CasperEventApplication.java
@@ -0,0 +1,14 @@
+package JGS.CasperEvent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+public class CasperEventApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CasperEventApplication.class, args);
+	}
+
+}
